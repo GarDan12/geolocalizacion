3a3bf312fa62d8463ee0a7a00775b7dc960f5a0e
GarDan12 <dannig201095@gmail.com>
2024-11-19 17:30:29 -0500

    first committ

--- src/app.js
+++ src/app.js
@@ -1,22 +1,26 @@
 import express from 'express';
-import cors from 'cors'; // Importar el paquete cors
+import cors from 'cors'; 
 import localizacionesRoutes from './routes/localizaciones.routes.js';
 
 const app = express();
 
-// Configurar CORS para permitir solicitudes desde http://localhost:8100
-app.use(cors({
-    origin: 'http://localhost:8100', // Reemplaza con el origen de tu aplicación cliente
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-    allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
-}));
 
-app.use(express.json());
+const corsOptions = {
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+    credentials: true, 
+};
 
-// Rutas
+// Middleware
+app.use(cors(corsOptions)); 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+
+
 app.use('/api', localizacionesRoutes);
 
-// Manejo de rutas no encontradas
+
 app.use((req, res, next) => {
     res.status(404).json({
         message: 'Endpoint not found',
